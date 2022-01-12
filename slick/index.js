79c4e0557e5672a940d4a94c54ca7e5b3d26f7f0
Anton Bekhtgold <jacepwncere@yandex.ru>
2022-01-12 17:46:18 +0300

    подключил slick

---
+++ slick/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+   $('.white-block__container__slider').slick({
+       arrows: true,
+       dots: false,
+       adaptiveHeight: true,
+       slidesToShow: 2, 
+       slidesToScroll: 1,
+       speed: 2000,
+       Infinite: true,
+       autoPlay: true,
+   });
+  });
